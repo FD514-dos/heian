591db2dc2b27bb93903075be95870e6ef47064f2
FD514-dos <tueanh132012@gmail.com>
2026-03-03 01:12:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.static("public"));
+
+let players = {};
+
+io.on("connection", (socket) => {
+
+  socket.on("login", (name) => {
+
+    // map tên thành 1 trong 15 char
+    const charId = hashName(name) % 15;
+
+    players[socket.id] = {
+      id: socket.id,
+      name,
+      charId,
+      x: Math.random() * 2000,
+      y: Math.random() * 2000
+    };
+
+    socket.emit("currentPlayers", players);
+    socket.broadcast.emit("newPlayer", players[socket.id]);
+  });
+
+  socket.on("move", (data) => {
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+      io.emit("playerMoved", players[socket.id]);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("playerDisconnected", socket.id);
+  });
+
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+function hashName(name) {
+  let hash = 0;
+  for (let i = 0; i < name.length; i++) {
+    hash += name.charCodeAt(i);
+  }
+  return hash;
+}
+
+server.listen(PORT, () => {
+  console.log("Server running");
 });
