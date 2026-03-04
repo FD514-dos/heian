fe077bd825da43c233ae790ebd1465298aa125bb
FD514-dos <tueanh132012@gmail.com>
2026-03-05 03:55:16 +0700

    thêm gì đó

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
+app.use("/assets", express.static("assets"));
 
 const allowedNames = [
 "Fyodor Dostoyevsky Dead Apple",
